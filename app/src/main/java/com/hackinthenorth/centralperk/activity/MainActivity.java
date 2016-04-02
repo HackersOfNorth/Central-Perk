90fe003b5a5d0d6313a97251bfac19f9773b5191
depp1993 <singh.deepankar93@gmail.com>
2016-04-02 08:01:16 +0530

    Improved login register UI

--- app/src/main/java/com/hackinthenorth/centralperk/activity/MainActivity.java
+++ app/src/main/java/com/hackinthenorth/centralperk/activity/MainActivity.java
@@ -1,13 +1,13 @@
 package com.hackinthenorth.centralperk.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.hackinthenorth.centralperk.R;
 
@@ -24,8 +24,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
             }
         });
     }
